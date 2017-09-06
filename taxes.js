51fefed01276ffa358a4f1904d54b9760b902aaf
Brandon Day <brandontday7@gmail.com>
2017-09-06 17:06:21 +0000

    layed out solution in psuedocode

---
+++ taxes.js
@@ -0,0 +1,54 @@
+var salesTaxRates = {
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
+
+var companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
+  }
+];
+
+
+
+function calculateSalesTax(salesData, taxRates) {
+  //   for (let i = 0; i < salesData.length; i++)
+  // {
+  //   let totalSales = 0;
+  //   for (let j = 0; j < salesData[i].sales.length; j++)
+  //   {
+  //     totalSales =
+
+  //   }
+  // }
+
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+// take in sales data
+// for the data in the array (normal for loop)
+//   add up the sales
+//   calculate the tax on the sales
+//   contribute these values to a new array, with objects having properties: totalSales, totalTaxes
+
+// create resultArray
+// for each new array element
+//     if we have seen the name before:
+//         add totalSales and totalTaxes to existing objects values
+//     else
+//         create a new object with values and continue
+
